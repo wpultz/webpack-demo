c876ac9839eb2a145ff593ca673a1c9b38376cc4
Wyatt Pultz <wyattpultz@Wyatts-MacBook-Pro.local>
2016-07-04 15:38:21 -0400

    add bootstrap

--- src/app1/app.js
+++ src/app1/app.js
@@ -3,13 +3,18 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 
+import Main from './containers';
+
 // auto-generated, attempt to get the initial state from another source, such as localStorage
 const initialState = {};
 const store = configureStore(initialState);
 
 render(
     <Provider store={store}>
-        <div>Hey this is a really cool app</div>
+        <div>
+            Hey this is the best app ever
+            <Main />
+        </div>
     </Provider>,
     document.getElementById('app1-cont')
 );
---
+++ src/app1/containers/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'react-bootstrap';
+
+const Main = () => {
+    return (
+        <div>
+            <p>an app goes here</p>
+            <Button>Really cool button</Button>
+        </div>
+    );
+};
+
+export default Main;
