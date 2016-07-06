3ce89b4dc642711d777703b48c6a12456bfa5c53
Wyatt Pultz <wyattpultz@Wyatts-MacBook-Pro.local>
2016-07-05 20:03:13 -0400

    simplify

--- webpack.config.js
+++ webpack.config.js
@@ -18,25 +18,7 @@ module.exports = {
         loaders: [
             { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel'] },
             { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
-            { test: /\.png$/, loader: 'url-loader?limit=100000' },
-            { test: /\.jpg$/, loader: 'file-loader' },
-            {
-                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/font-woff'
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/octet-stream'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file'
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=image/svg+xml'
-            }
+            { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' }
         ]
     },
     resolve: {
